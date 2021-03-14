b6a447fd9667f91ea285dc485f743c2b4fe07f2b
marc <marc@Formation.home>
2021-03-14 21:57:04 +0100

    Le script récupère desormais les infos pour toutes les catégories

--- projet2.py
+++ projet2.py
@@ -1,5 +1,5 @@
 """
-  Projet 2
+  Projet 2: Analyse de marché
 """
 # -*- coding: utf-8 -*-
 from urllib.parse import urlparse
@@ -9,37 +9,35 @@ import os
 import requests
 from bs4 import BeautifulSoup
 
-def getinfobook(product_page_url):
-    """ Récupere les informations d'un livre depuis l'url du livre
-        la fonction renvoie les informations sous forme de dictionaire
-        les 10 clefs qui compose ce dictionnaire:
-          - product_page_url
-          - universal_product_code
-          - title
-          - price_including_tax
-          - price_excluding_tax
-          - number_available
-          - product_description
-          - category
-          - review_rating
-          - image_url
-       La fonction BeautifulSoup récupère les donnéés avec les tags suivants:
-          - td  => ( il y a 7 td sur une page )
-            - td N°1                           => universal_ product_code (upc)
-            - td N°3 et N°4                    => price_including_tax et price_excluding_tax
-            - td N°6                           => number_available
-            - td N°7                           => review_rating
-          - li et "class":"active"             => title
-          - meta + "name":"description"        => product_description
-          - a +regex de  "../category/books/"  => category
-          - img + alt "Nom du livre"           => image_url
+def infobook(product_page_url):
+    """ Récupère les informations d'un livre depuis l'url du livre
+        La fonction renvoie les informations sous forme de dictionnaire
+        les 10 clefs qui composent ce dictionnaire :
+            - product_page_url
+            - universal_product_code
+            - title
+            - price_including_tax
+            - price_excluding_tax
+            - number_available
+            - product_description
+            - category
+            - review_rating
+            - image_url
+        La fonction BeautifulSoup récupère les données avec les tags suivants:
+            - td => ( il y a 7 td sur une page )
+            - td N°1 => universal_ product_code (upc)
+            - td N°3 et N°4 => price_including_tax et price_excluding_tax
+            - td N°6 => number_available
+            - td N°7 => review_rating
+            - li et "class":"active" => title
+            - meta + "name":"description" => product_description
+            - a +regex de "../category/books/" => category
+            - img + alt "Nom du livre" => image_url
     """
     infobook = {}
-    # On ajoute la ref url
     infobook['product_page_url'] = product_page_url
     response = requests.get(product_page_url)
-    parsed_uri = urlparse(product_page_url)
-    url_site = '{uri.scheme}://{uri.netloc}/'.format(uri=parsed_uri)
+    url_site = geturlsite(product_page_url)
     if response.ok:
         soup = BeautifulSoup(response.text, 'html.parser')
         tds = soup.findAll('td')
@@ -56,19 +54,24 @@ def getinfobook(product_page_url):
         infobook['review_rating'] = tds[6].text
         infobook['image_url'] = url_site + \
             soup.find('img', {"alt": title})['src'].replace('../', '')
-        writecsvcategory(infobook)
+    return infobook
 
-def writecsvcategory(data):
-    """ Créé le reprtoire de destination si absent
-        Ecrit les entêtes du csv puis puis écrit les données
+def geturlsite(url):
+    """ Renvoie l'adresse du site """
+    parsed_uri = urlparse(url)
+    return '{uri.scheme}://{uri.netloc}/'.format(uri=parsed_uri)
+
+def writecsvcategory(datarray):
+    """ La fonction créée le répertoire de destination si absent
+        Ecrit les entêtes du csv puis les données
         Le fichier csv sera stocké dans le répertoire
         repdata+/<category>/+<category>.csv
     """
     repbase = '/tmp/data/csv/'
-    repdata = repbase+data['category']
+    repdata = repbase+datarray[0]['category']
     if not os.path.exists(repdata):
         os.makedirs(repdata)
-    file_csv=repdata+'/'+data['category']+'.csv'
+    file_csv = repdata+'/'+datarray[0]['category']+'.csv'
     fieldnames = ['product_page_url', 'universal_product_code (upc)', 'title',
                   'price_including_tax', 'price_excluding_tax', 'number_available',
                   'product_description', 'category', 'review_rating', 'image_url']
@@ -78,8 +81,78 @@ def writecsvcategory(data):
                                 quoting=csv.QUOTE_MINIMAL
                                 )
         writer.writeheader()
-        writer.writerow(data)
+        for data in datarray:
+            writer.writerow(data)
+
+def infobookbycategory(category_page_url):
+    """ La fonction récupère les urls des livres d'une catégorie
+        avec la fonction urlbookbycategory().
+        Avec les "URLs Livre" la fonction infobook() obtient les informations demandées
+        pour chaque livre puis on enregistre les données de la catégorie en
+        format csv avec la fonction writecsvcategory """
+    databook = []
+    urlsbook = urlbookbycategory(category_page_url)
+    for urlbook in urlsbook:
+        databook.append(infobook(urlbook))
+    writecsvcategory(databook)
+
+def urlbookbycategory(category_page_url):
+    """ Cette fonction renvoie les urls des livres pour une catégorie
+        1) on récupère les pages liées à une catégorie avec la fonction pagebycategory()
+        2) Pour chaque page on récupère les liens des articles présents
+        3) On renvoie les liens de tous les livres liés à une catégorie sous forme de liste
+    """
+    urls = pagebycategory(category_page_url)
+    urlsite = geturlsite(category_page_url)
+    urlsbook = []
+    for url in urls:
+        response = requests.get(url)
+        soup = BeautifulSoup(response.text, 'html.parser')
+        for urlbook in soup.findAll('h3'):
+            if urlbook.find('a'):
+                urlbook = urlsite + \
+                    urlbook.find('a')['href'].replace(
+                        '../../../', 'catalogue/')
+                urlsbook.append(urlbook)
+    return urlsbook
+
+def pagebycategory(category_page_url):
+    """ Renvoie la liste des pages pour une catégorie
+        1) La fonction request récupère le code html d'une page "catégorie"
+        2) La fonction BeautifulSoup le nombre de pages liées à une catégorie les tags "li"
+           et la class current  =>  soup.find('li',{'class':'current'}
+        La page par défaut est index.html les pages suivantes si elles existent sont
+        page-2.html page-3.html ....
+        si aucune page détectée on renvoie la page par défaut
+        sinon on renvoie la page par défaut +les pages  page-x.html
+        pages = nbr de pages trouvées -1   (le -1 étant la page par défault)
+        On commence avec un index à +2 pour les pages
+    """
+    response = requests.get(category_page_url)
+    soup = BeautifulSoup(response.text, 'html.parser')
+    pagin = soup.find('li', {'class': 'current'})
+    listpagebycategory = []
+    listpagebycategory.append(category_page_url)
+    if pagin:
+        pages = int(pagin.text.strip().split(' ')[3])-1
+        category_url=category_page_url.replace('index.html', '')
+        for i in range(pages):
+            listpagebycategory.append(category_url+'page-'+str(i+2)+'.html')
+        return listpagebycategory
+    return listpagebycategory
+
+def listcategory(url):
+    """ Liste les catégories en utilisant les tags 'ul' et class=='nav nav-list'
+        puis envoi les urls des catégories du site à la fonction infobookbycategory() pour
+        un traitement sur les catégories.
+    """
+    response = requests.get(url)
+    soup = BeautifulSoup(response.text, 'html.parser')
+    category_urls = soup.find('ul', {'class': 'nav nav-list'}).findAll('li')
+    del category_urls[0]
+    for category in category_urls:
+        category_url=url+category.find('a')['href']
+        infobookbycategory(category_url)
 
 if __name__ == '__main__':
-    URL = 'http://books.toscrape.com/catalogue/sharp-objects_997/index.html'
-    getinfobook(URL)
+    listcategory(url='http://books.toscrape.com/')
